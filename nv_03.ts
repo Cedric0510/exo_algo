650d47895efe016a451de0449415777c0f9e26a6
Cedric0510 <cedric.vanhove.82@gmail.com>
2024-10-10 16:47:16 +0200

    nv_03 Ex A fini

---
+++ nv_03.ts
@@ -0,0 +1,14 @@
+// (A) Écrire une fonction hello qui demande à l’utilisateur d’entrer son nom et affiche une salutation
+// reprenant son nom.
+
+function hello(name:string):string{
+    let hello:string = "Hello";
+    let yourname:string = name;
+
+let salute = `hello ${name} tu es beau!!!`;
+
+return salute
+
+}
+
+console.log(hello("cédric"));
