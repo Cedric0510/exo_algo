4e1d34d688ddc1c4acb621bbff8756bca4a03e74
Cedric0510 <cedric.vanhove.82@gmail.com>
2024-10-10 16:38:55 +0200

    Exercice Nv2 E fini

--- nv_2.ts
+++ nv_2.ts
@@ -11,7 +11,7 @@ function h_triangle (h:number):string{
     return res
 };
 
-console.log(h_triangle(4))
+// console.log(h_triangle(4))
 
 
 // (B) Écrire une fonction rectangle qui prend deux entiers n et m et qui renvoie un unique string
@@ -27,7 +27,7 @@ function rectangle (n:number, m:number): string{
     return res
 }
 
-console.log(rectangle(5,3))
+// console.log(rectangle(5,3))
 
 //(C) Écrire une fonction reverse_ints qui prend un entier n et qui renvoie un string faisant le
 // décompte inverse des entiers compris entre 1 et n inclus.
@@ -42,7 +42,34 @@ function reverse_ints(n:number):string{
     return res;
 }
 
-console.log(reverse_ints(6));
+// console.log(reverse_ints(6));
 
 //(D) Écrire une fonction int_pyramid qui prend un entier n et qui renvoie un string constituant un
 // triangle qui indique à chaque emplacement la longueur de la pyramide.
+
+function int_pyramid(n:number):string{
+    let res:string="";
+    let line:string="";
+    for(let i=0; i<n; i++){
+        line += (i+1);
+        res += line + "\n"        
+}
+
+return res
+    
+}
+
+// console.log(int_pyramid(8));
+
+//(E) Écrire une fonction beautiful_mult qui prend un entier n et renvoie à l’aide d’un unique string
+// sa table de multiplication formatée.
+
+function beautiful_mult(n:number):string{
+    let line: string = "";
+    for(let i=0; i<11; i++){
+        line += `${n}*${i}=${i*n},`;
+    }
+    return line
+}
+
+console.log(beautiful_mult(4))
