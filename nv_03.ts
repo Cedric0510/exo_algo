1d4b891e98f1e05f50ab6c77919c33a8574b59a8
Cedric0510 <cedric.vanhove.82@gmail.com>
2024-10-24 11:36:58 +0200

    Nv3 Cleared

--- nv_03.ts
+++ nv_03.ts
@@ -3,6 +3,7 @@
 
 import { equal } from "assert";
 import { parentPort } from "worker_threads";
+import * as readlineSync from 'readline-sync';
 
 function hello(name:string):string{    
     let readlineSync = require('readline-sync');
@@ -39,39 +40,74 @@ function avg_inputs():number{
 
 // console.log(avg_inputs())
 
-//Écrire une fonction RPS qui demande à l’utilisateur de rentrer au choix “Rock”, “Paper” ou
-// “Scissor”. À l’aide de la fonction Math.random(), faites également choisir votre programme et
-// indiquez au joueur s’il a gagné ou perdu. Recommencer jusqu’à ce que le joueur gagne.
 
-function RPS():boolean{
-    let win : boolean = false;
-    let readlineSync = require('readline-sync');
-    let input = readlineSync.question('Rock, Paper, Scissor : ')
-    let player : string = input;
-    let Ia = Math.random(3);
-    let Rock:number = 3
-    let Paper:number = 1
-    let Scissor:number = 2
-    let array : string[] = ["Rock, Paper, Scissor" ]
-//     switch(){
-//         case{
-//         Rock>Scissor
-//         return Rock WIN
-//     }case{
-//         Scissor>Paper
-//         return Scissor WIN 
-//     }case{
-//         Paper>Rock 
-//         return Paper WIN 
-//     }case{
-//         Rock=Rock 
-//         Return Equal
-//     }case{
-//         Scissor=Scissor 
-//         Return Equal
-//     }case{
-//         Paper=Paper 
-//         Return Equal 
-//     }
-// return unTruc
-// }
+
+function RPS():string{
+  const items: string[] = ["Rock", "Paper", "Scissor"];
+  let userChoice : string = readlineSync.question("Your Choose (Rock,Paper,Scissor)");
+  const length = items.length
+  let ItemIndex : number = Math.floor(Math.random() * length);
+  const index = items.findIndex((playerChoice) => playerChoice === userChoice);
+  let playerChoice = index;
+
+  console.log(ItemIndex)
+  if (playerChoice !== ItemIndex){
+    if(((playerChoice - ItemIndex)+length) %length == 0){
+      return "You win"
+
+    }else{
+      return "You Loose"
+    }
+  }
+  return "Tie"
+};
+
+// console.log(RPS())
+
+
+// Guess a Number.
+// L’objectif est de choisir aléatoirement un nombre compris entre 1 et 1000 et de faire deviner à
+// l’utilisateur jusqu’à ce qu’il trouve, lui indiquant à chaque fois s’il a deviné “trop grand” ou
+// “trop petit”. Bonus : compter en combien de coups le joueur a deviné.
+
+function GuessANumber():string{
+let numberComp : number = Math.floor(Math.random() * 1000);
+let numberUser : number = readlineSync.questionInt("Your Number between 1 & 1000 : ");
+
+  while (numberComp !== numberUser){
+    if(numberComp>numberUser){
+       numberUser = readlineSync.questionInt("Up : ");
+    }else{
+      numberUser = readlineSync.questionInt("Down : ");
+    }
+  }
+  return"Win"
+}
+
+// console.log(GuessANumber())
+
+
+// Mysterious Mult.
+// Dans la même idée, on choisit un nombre aléatoire compris entre 1 et 5000. L’objectif à
+// nouveau est de le faire deviner à l’utilisateur, seulement cette fois ci on répond uniquement
+// par True si le nombre deviné est un diviseur, sinon False
+
+
+function MysteriousMult():string {
+  let numberComp : number = Math.floor(Math.random() * 5000);
+  console.log(numberComp)
+  let numberUser : number = readlineSync.questionInt("Your Number between 1 & 5000 : ");
+  
+
+  while(numberComp !== numberUser){
+    if(numberComp % numberUser == 0){
+      numberUser = readlineSync.questionInt("True \n Play again : ");
+    }else{
+      numberUser = readlineSync.questionInt("False \n Play again : ");
+    }  
+  }
+  return "You WIn"
+}
+
+
+console.log(MysteriousMult())
